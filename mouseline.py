7a428a7f20c9a1f88838abb06dee611944e32d00
Tanaya <tanaya.palandurkar@gmail.com>
2021-09-14 08:59:12 +0200

    modefied mouseline for ones kernel

--- mouseline.py
+++ mouseline.py
@@ -15,9 +15,9 @@ def click_event(event, x, y, flags, params):
         if len(points)>= 2:
             cv2.line(img, points[-1], points[-2], (0, 0, 255), 5)
         cv2.imshow("image", img)
-        
-        
-img = np.zeros((512, 512, 3), np.uint8)
+
+
+img = np.ones((512, 512, 3), np.uint8)
 
 cv2.imshow("image", img)
 points =[]
